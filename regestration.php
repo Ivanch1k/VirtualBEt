dfe442f362ffea6f0de7a030f61760ab3e72a70b
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-05-02 17:17:52 +0300

    Add files via upload

---
+++ regestration.php
@@ -0,0 +1,10 @@
+<header>
+	
+</header>
+<body>
+	<form method="POST" action="\registration.php">
+	<input type="input" name="name"><br>
+	<input type="input" name="secondName"><br>
+	<input type="submit">
+	</form>
+</body>
