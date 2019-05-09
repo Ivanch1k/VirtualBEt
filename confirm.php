d07428609bfe49c94e06eb6170e1d4b7c55eaab1
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-05-09 13:48:21 +0300

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -3,11 +3,11 @@
 	$link = pg_connect(getenv("DATABASE_URL"));
 	
 	if($link == 0){
-		echo'Cannot connect to database;';
+		echo'Cannot connect to database';
 		return;
 	}
 	
-	$sql = "select all from test_table";
+	$sql = "select all from test_table;";
 	$result = pg_query($link, $sql);
 	$res = pg_fetch_all($result);
 	print_r($res);
