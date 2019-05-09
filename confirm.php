ab3a421e271a25523fbeea0b1a3d0b25d400aba0
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-05-09 13:27:09 +0300

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -1,5 +1,14 @@
 <?php 
-	echo "You'r name is $_GET[name]";
-	echo '<br>';
-	echo "You'r lastname is $_GET[secondName]";
+	$link = 0;
+	$link = pg_connect(getenv("DATABASE_URL"));
+	
+	if($link == 0){
+		echo'Cannot connect to database';
+		return;
+	}
+	
+	$sql = "select all from test_table";
+	$result = pg_query($link, $sql);
+
+	echo $result;
 ?>
