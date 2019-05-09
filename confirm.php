6e78c84be481b103bdb9fe5a60c1fa4390b09f51
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-05-09 16:06:24 +0300

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -1,5 +1,4 @@
 <?php 
-	$link = 0;
 	$link = pg_connect(getenv("DATABASE_URL"));
 	if($link == false){
 		echo'Fault connection';
@@ -8,7 +7,14 @@
 
 	$sql = "select * from test_table";
 	$result = pg_query($link, $sql);
-	
 	$res = pg_fetch_all($result);
-	echo sizeof($res);
+	
+	foreach($res as $a){
+		if($a['login'] == $_GET['login']){
+			echo 'Glade to see you again!';
+		}
+	}
+	else{
+		echo "We don't meet before";
+	}
 ?>
