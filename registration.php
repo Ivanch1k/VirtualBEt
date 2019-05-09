caaf91e1d3392c0c3034cdf8897d00b4bc21e083
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-05-09 17:27:49 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,7 @@
   <form method="GET" action="\confrim.php">
     <input type="text" name="login">
     <input type="text" name="password">
+    <input type="text" name="type" value="reg" style="display: none">
     <input type="submit">
   </form>
 </body>
