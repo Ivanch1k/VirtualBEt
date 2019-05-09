cecf1736f3df928cea3b23c2c87d78e5e9f08be4
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-05-09 13:59:31 +0300

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -1,11 +1,17 @@
 <?php 
 	$link = 0;
 	$link = pg_connect(getenv("DATABASE_URL"));
-	
-	var_dump($link);	
+	if($link == false){
+		echo'Fault connection';
+		return;
+	}
 
-	$sql = "select all from test_table;";
+	$sql = "insert into test_table values(2, 'Ivanchik', '1234')";
+	pg_query($link, $sql);
+	$sql = "select all from test_table";
 	$result = pg_query($link, $sql);
+	
 	$res = pg_fetch_all($result);
 	print_r($res);
+	print_r($result);
 ?>
