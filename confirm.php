9776d7b5e53857d83b38ca015b7123c96643ce13
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-05-09 17:45:52 +0300

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -31,7 +31,10 @@ else if($_GET['type'] == 'reg'){
 		$sql = "select MAX(id) from test_table group by id";
 		$nextNumber = pg_query($link, $sql) + 1;
 		$sql = "insert into test_table values(" . $nextNumber . ", " . $_GET['login'] . ", " . $_GET['password'] . ")";
+		echo $sql;
 		$result = pg_query($link, $sql);
+		echo "Welcome to our site!";
 	}
+	echo "Login or passwort are empty";
 }
 ?>
