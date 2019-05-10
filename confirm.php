d72bf98a7d7b2f6becc0585064c01fab538a91be
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-05-10 13:08:29 +0300

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -34,7 +34,6 @@ else if($_GET['type'] == 'reg'){
 		$sql = "insert into test_table (id, login, password) values($nextNumber, '{$_GET['login']}', '{$_GET['password']}')";
 		$result = pg_query($link, $sql);	
 		$res = pg_fetch_all($result);
-		var_dump($res);
 		echo "Welcome to our site!";
 	}
 	else{
