b88da74197482afa3b6b24cfa88c20b098e8c931
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-01 11:13:02 +0300

    Update testServer.php

--- testServer.php
+++ testServer.php
@@ -1,2 +1,14 @@
-<? php 
+<?php
+	$link = pg_connect(getenv("DATABASE_URL"));
+	if($link == false){
+		echo'Fault connection';
+		return;
+	}
+	
+	if($_GET['action'] != null){	
+		if($_GET['action'] == 'SeyHello'){
+			echo('Hello');
+		}
+	}
+	
 ?>
