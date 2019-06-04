f61f1c44c5d55cb1fcc1be7293d79f110fd0520d
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-04 21:25:36 +0300

    Update testServer.php

--- testServer.php
+++ testServer.php
@@ -1,11 +1,6 @@
-<?php
-	$login = $_POST['login'];
-	$passwodr = $_POST['password'];
-		
-	$password = password_hash($passwodr, PASSWORD_DEFAULT);
-		
-	$result["success"] = "1";
-	$result["message"] = "Finnaly Hello";
+<?php	
+	$result["login"] = "1";
+	$result["password"] = "Finnaly Hello";
 		
 	echo json_encode($result);
 	
