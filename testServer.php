e1713d35abd36e7d72f2641584207c1c94ab4901
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-06 19:40:01 +0300

    Update testServer.php

--- testServer.php
+++ testServer.php
@@ -1,8 +1,11 @@
 <?php	
+	$result["status"] = "fail";
 	if($_GET['login'] == 'Admin' && $_GET['password'] == '1423'){
+		$result["status"] = "success";
 		$result["login"] = "Admin";
 		$result["password"] = "1423";
 	} else if($_GET['login'] == '123'){
+		$result["status"] = "success";
 		$result["login"] = "Admin";
 		$result["password"] = "It's work tooo!!!";
 	}
