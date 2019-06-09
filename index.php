d853513e309dd1f510a227a0eae4849b6941f162
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-09 16:18:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
-  <?php	
-	$result["success"] = "1";
-	$result["message"] = "Finnaly Hello";	
+<?php
+	$link = pg_connect(getenv("DATABASE_URL"));
+	if($link == false){
+		echo json_encode('status=connection_failed');
+		return;
+	} else if($_GET['action'] == 'login'){
+		$sql = "select * from client where Mail=".$_GET['login']." and Pass=".$_GET['password'];
+		
+		$response = pg_query($link, $sql);
+		if($response != ''){
+			echo $response;
+			foreach($response as $key => $value){
+		
+			}
+			$result = pg_fetch_all($response);
+		}
+	} else if($_GET['action'] == 'matches'){
+		$sql = "select * from matches";
+		$response = pg_query($link, $sql);
+		echo $response;
+	}
+	echo '--------------------------';
 	echo json_encode($result);
 ?>
