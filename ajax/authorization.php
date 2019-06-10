bb404b5260a8a01eff4a7f7a2f73abb3ae3b5a16
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-10 21:15:14 +0300

    Update authorization.php

--- ajax/authorization.php
+++ ajax/authorization.php
@@ -1,18 +1,24 @@
 <?php
 session_start();
-function match($result, $mail, $pass){
+/*function match($result, $mail, $pass){
     while(($row = $result->fetch_assoc()) != false){
         if($row["Mail"] == $mail && $row["Pass"] == $pass){
             return $row;
         }
     }
     return false;
-}
+}*/
 $email = $_POST['email'];
 $pass = md5($_POST['pass']);
 if($email != 'ogteam'&& $pass !='123456') {
     $mysql = pg_connect(getenv("DATABASE_URL"));
-    $result = pg_fetch_all(pq_query($mysql,"SELECT * FROM client;"));
+    $results = pg_fetch_all(pq_query($mysql,"SELECT * FROM client;"));
+    $user = false;
+    foreach ($results as $result){
+        if($result['Mail'] == $email && $result['Pass'] == "$pass"){
+            $user = true;
+        }
+    }
 
     $user = match($result, $email, $pass);
     if ($user != false) {
