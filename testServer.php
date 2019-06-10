2028b7aa6d791077031e27c483b3b96dfa9b4f39
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-10 20:32:59 +0300

    Update testServer.php

--- testServer.php
+++ testServer.php
@@ -25,6 +25,18 @@
 			//echo 'empty';
 		}		
 		echo json_encode($result);
+	} else if($_GET['action'] == 'insert'){
+		$sql = "INSERT INTO client(Name, SecondName, DateOfBirth, Mail, PhoneNumber, Pass, Balance) VALUES('name','secondName','1999-01-01','email','number','pass',300);";
+		if(!$sql){
+			echo('Insern returns false');
+		}
+		$response = pg_query($link, $sql);
+		if($response != ''){
+			$result = pg_fetch_all($response);
+		} else {
+			//echo 'empty';
+		}		
+		
 	}
 	
 ?>
