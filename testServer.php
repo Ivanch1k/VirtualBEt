bf3b84838c87a819d9da3746ca69057948edbac6
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-10 21:50:23 +0300

    Update testServer.php

--- testServer.php
+++ testServer.php
@@ -1,1 +1,16 @@
 
+<?php	
+	$link = pg_connect(getenv("DATABASE_URL"));
+	if($link == false){
+		echo json_encode('status=connection_failed');
+		return;
+	} else if($_GET['action'] == 'login'){
+		$sql = "select * from client where Mail=".$_GET['login']." and Pass=".$_GET['password'].";";
+		
+		$response = pg_query($link, $sql);
+		if($response != ''){
+			$result = pg_fetch_all($response);
+		} 
+	}
+	echo json_encode($result);
+?>
