c9779e795a520b4a3a6e2c0506ad7a02973dc931
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-10 21:40:10 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -3,7 +3,7 @@ $mysql = pg_connect(getenv("DATABASE_URL"));
     $results = pg_fetch_all(pg_query($mysql,"SELECT * FROM client;"));
     $user = false;
     foreach ($results as $result){
-        print_r(pg_fetch_all($result));
+        print_r($result);
         if($result['Mail'] == $email && $result['Pass'] == "$pass"){
             $user = true;
         }
