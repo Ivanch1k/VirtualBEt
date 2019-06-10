ac4cd4f783bbd5fd30fbc57cb17620e97b3ae0a4
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-11 02:58:39 +0300

    Update testServer.php

--- testServer.php
+++ testServer.php
@@ -16,7 +16,10 @@
 		$response = pg_fetch_all(pg_query($link, $sql));
 		echo $response;
 		if($response == "null"){
-			$sql = "insert into client values('".$_GET['name']."', '".$_GET['surname']."', '".$_GET['dateOfBirth']."', '".$_GET['login']."', '', '".$_GET['password']."', '', 1000);";
+			$sql = "select MAX(id) from client;";
+			$response = pg_fetch_all(pg_query($link, $sql));
+			$responce += 1;
+			$sql = "insert into client values('".$response."', '".$_GET['name']."', '".$_GET['surname']."', '".$_GET['dateOfBirth']."', '".$_GET['login']."', '', '".$_GET['password']."', '', 1000);";
 			$response = pg_query($link, $sql);
 			if(!$response){
 				echo '{"status":"0"}';
@@ -30,6 +33,8 @@
 			return;
 		}
 	} else if($_GET['action'] == 'test'){
+		$sql = "select MAX(id)  test values(2, 'admin', '1999-01-01');";
+		$response = pg_query($link, $sql);
 		$sql = "insert into test values(2, 'admin', '1999-01-01');";
 		$response = pg_query($link, $sql);
 		echo $response;
