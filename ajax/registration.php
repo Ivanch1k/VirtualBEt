204c78bb0cb79b13c1c4bcff66fbd667c52ca625
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-10 21:28:19 +0300

    Update registration.php

--- ajax/registration.php
+++ ajax/registration.php
@@ -11,14 +11,14 @@
 
 $mysql = pg_connect(getenv("DATABASE_URL"));
 //существуует ли пользователь
-$client = pq_query($mysql,"SELECT * FROM client WHERE Mail = '$email';");
+$client = pg_query($mysql,"SELECT * FROM client WHERE Mail = '$email';");
 $emailCounter = 0;
 foreach ($client as $c){
     $emailCounter++;
 }
 
 // номер телефона уже зареган в базе
-$client = pq_query($mysql,"SELECT * FROM client WHERE PhoneNumber = '$number';");
+$client = pg_query($mysql,"SELECT * FROM client WHERE PhoneNumber = '$number';");
 $numberCounter = 0;
 foreach ($client as $c){
     $numberCounter++;
@@ -31,9 +31,9 @@ if($emailCounter > 0){
     echo "NumberExistException";
 } else {
     if ($number != "+380") {
-        $result = pq_query($mysql,"INSERT INTO client(Name, SecondName, DateOfBirth, Mail, PhoneNumber, Pass, Balance) VALUES('$name','$secondName','$date','$email','$number','$pass',300);");
+        $result = pg_query($mysql,"INSERT INTO client(Name, SecondName, DateOfBirth, Mail, PhoneNumber, Pass, Balance) VALUES('$name','$secondName','$date','$email','$number','$pass',300);");
     } else {
-        $result = pq_query($mysql,"INSERT INTO client(Name, SecondName, DateOfBirth, Mail, Pass, Balance) VALUES('$name','$secondName','$date','$email','$pass',300);");
+        $result = pg_query($mysql,"INSERT INTO client(Name, SecondName, DateOfBirth, Mail, Pass, Balance) VALUES('$name','$secondName','$date','$email','$pass',300);");
     }
     echo"https://virtualbet.herokuapp.com/index.php";
 }
