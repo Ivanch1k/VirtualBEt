0e1814ce5d1c671517320b88bfb93cbc2f039fd1
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-10 21:59:15 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 $mysql = pg_connect(getenv("DATABASE_URL"));
-    $results = pg_fetch_all(pg_query($mysql,"SELECT * FROM client;"));
+    $results = pg_query($mysql,"SELECT * FROM client;");
     $user = false;
     foreach ($results as $result){
         echo $result['Mail'];
