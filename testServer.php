2c73e2c81fad51b6b375275a749b4fb77ff442e9
Ivan <48558279+Ivanch1k@users.noreply.github.com>
2019-06-11 03:16:46 +0300

    Update testServer.php

--- testServer.php
+++ testServer.php
@@ -15,27 +15,7 @@
 		$sql = "select * from client where Mail='".$_GET['login']."' and Pass='".$_GET['password']."';";
 		$response = pg_fetch_all(pg_query($link, $sql));
 		echo $response;
-		if($response == "null"){
-			$sql = "select MAX(id) from client;";
-			echo 'sfdfg';
-			$response = pg_fetch_all(pg_query($link, $sql));
-			echo 'sfdfg';
-			echo $response;
-			$response += 1;
-			echo $response;
-			$sql = "insert into client values(".$response.", '".$_GET['name']."', '".$_GET['surname']."', '".$_GET['dateOfBirth']."', '".$_GET['login']."', '', '".$_GET['password']."', '', 1000);";
-			$response = pg_query($link, $sql);
-			if(!$response){
-				echo '{"status":"0"}';
-				return;
-			}
-		} else if(!$response){
-			echo 'error in sql';
-			return;
-		} else{
-			echo '{"status":"1"}';
-			return;
-		}
+		
 	} else if($_GET['action'] == 'test'){
 		$sql = "select MAX(id)  test values(2, 'admin', '1999-01-01');";
 		$response = pg_query($link, $sql);
